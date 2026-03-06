e8154b8980abe588e84d341d6ec6b91e9f99cceb
lindomaralvesdossantosalvesdos-byte <lindomaralvesdossantosalvesdos@gmail.com>
2026-03-05 21:51:54 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+// Matrix TLL - Service Worker v4
+const CACHE = 'mtll-v4';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(
+      ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
